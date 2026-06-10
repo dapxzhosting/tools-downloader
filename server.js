4f64040c99bfb62da82c90a264bc97ac80262bbd
Dafa Alfiansyah <alfiansyahdafa10@gmail.com>
2026-06-10 16:45:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
 const os = require("os");
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
